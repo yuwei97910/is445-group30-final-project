b3dd88a823ed7eb5e6e8b338b587518076c92598
Thomas Huang <yenshouhuang@gmail.com>
2022-12-01 14:49:10 -0600

    change font

--- dashboard.js
+++ dashboard.js
@@ -3,11 +3,11 @@ var dlink =
 $("#map").usmap({
   // The click action
   click: function (event, data) {
-    $("#clicked-state").text("You clicked: " + data.name);
+    $("#clicked-state").text("You have selected this state: " + data.name);
     dlink = `https://raw.githubusercontent.com/yuwei97910/is445-group30-final-project/main/DataSet/yelp_academic_dataset_business_${data.name}.json`;
     console.log(data.name);
     console.log(dlink);
-    finalSpec = {
+    finalViz = {
       $schema: "https://vega.github.io/schema/vega-lite/v5.json",
       data: { url: dlink },
       mark: "bar",
@@ -20,10 +20,10 @@ $("#map").usmap({
         color: { value: "orange" },
       },
     };
-    vegaEmbed("#vis", finalSpec);
+    vegaEmbed("#vis", finalViz);
   },
 });
-finalSpec = {
+finalViz = {
   $schema: "https://vega.github.io/schema/vega-lite/v5.json",
   data: { url: dlink },
   mark: "bar",
@@ -36,4 +36,4 @@ finalSpec = {
     color: { value: "orange" },
   },
 };
-vegaEmbed("#vis", finalSpec);
+vegaEmbed("#vis", finalViz);
