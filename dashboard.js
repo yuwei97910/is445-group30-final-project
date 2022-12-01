046f5d8fc020a9ba86d7ae1add5c3342f7a86ed2
Thomas Huang <yenshouhuang@gmail.com>
2022-12-01 10:41:11 -0600

    fix dashboard async

---
+++ dashboard.js
@@ -0,0 +1,39 @@
+var dlink =
+  "https://raw.githubusercontent.com/yuwei97910/is445-group30-final-project/main/DataSet/yelp_academic_dataset_business_IL.json";
+$("#map").usmap({
+  // The click action
+  click: function (event, data) {
+    $("#clicked-state").text("You clicked: " + data.name);
+    dlink = `https://raw.githubusercontent.com/yuwei97910/is445-group30-final-project/main/DataSet/yelp_academic_dataset_business_${data.name}.json`;
+    console.log(data.name);
+    console.log(dlink);
+    finalSpec = {
+      $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+      data: { url: dlink },
+      mark: "bar",
+      height: 300,
+      width: 1000,
+      background: null,
+      encoding: {
+        x: { field: "stars", type: "ordinal" },
+        y: { aggregate: "count", field: "stars", type: "quantitative" },
+        color: { value: "orange" },
+      },
+    };
+    vegaEmbed("#vis", finalSpec);
+  },
+});
+finalSpec = {
+  $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+  data: { url: dlink },
+  mark: "bar",
+  height: 300,
+  width: 1000,
+  background: null,
+  encoding: {
+    x: { field: "stars", type: "ordinal" },
+    y: { aggregate: "count", field: "stars", type: "quantitative" },
+    color: { value: "orange" },
+  },
+};
+vegaEmbed("#vis", finalSpec);
